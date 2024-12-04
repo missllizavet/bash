8c5d5acad3258f6b0f0816834f5c18700de3ef5e
missllizavet <liza.nikitina2018@yandex.ru>
2024-12-04 16:42:23 +0300

    added lab1

---
+++ task.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Проверка существования файла .bashrc
+if [ ! -f "$HOME/.bashrc" ]; then
+  echo "File ~/.bashrc not found." >&2
+  exit 1
+fi
+
+# Извлечение строк с 12 по 45
+head -n 45 "$HOME/.bashrc" | tail -n +12 > test.txt
+
+# Проверка на ошибки
+if [ $? -eq 0 ]; then
+  echo "Strings succesfull saves in test.txt"
+else
+  echo "Error!" >&2
+  exit 1
+fi
